c79f75a00883b6a7d1e308ff4bd17077964578f2
Alejandro Nanez <alejonanez@gmail.com>
2020-07-16 22:56:52 -0500

    Add FederatedWrapper component

--- packages/blog/src/App.js
+++ packages/blog/src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FederatedWrapper from './FederatedWrapper';
 
 const Button = React.lazy(() => import('designSystem/Button'));
-const Avatar = React.lazy(() => import('designSystem/Avatar'));
 const Navigation = React.lazy(() => import('navigation/Navigation'));
 
 const App = () => {
   return (
     <div>
-      <React.Suspense fallback={<div>Loading...</div>}>
+      <FederatedWrapper
+        error={<div>Fallback Navigation</div>}
+        delayed={<div>Loading Navigation...</div>}
+      >
         <Navigation />
+      </FederatedWrapper>
+      <FederatedWrapper
+        error={<div>Fallback Button</div>}
+        delayed={<div>Loading Button...</div>}
+      >
         <Button>I am the button from another remote - localhost:3001</Button>
-        <p>I am a paragraph from localhost:3000</p>
-      </React.Suspense>
+      </FederatedWrapper>
+      <p>I am a paragraph from localhost:3000</p>
     </div>
   );
 };
---
+++ packages/blog/src/FederatedWrapper.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class FederatedWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  componentDidCatch(error, errorInfo) {
+    // logErrorToMyService(error, errorInfo);
+  }
+  render() {
+    if (this.state.hasError) {
+      return this.props.error || <h1>Something went wrong.</h1>;
+    }
+
+    return (
+      <React.Suspense fallback={this.props.delayed || <div />}>
+        {this.props.children}
+      </React.Suspense>
+    );
+  }
+}
+
+export default FederatedWrapper;
